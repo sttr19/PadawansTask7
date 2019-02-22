5625388c7567f38d10246c5b659027f240435823
sttr19 <stastroshko@gmail.com>
2019-02-22 09:52:29 +0300

    Update StringExtension.cs

--- StringExtension.cs
+++ StringExtension.cs
@@ -6,8 +6,19 @@ namespace PadawansTask7
     {
         public static void OrderStringsByLength(string[] array)
         {
-            // put your code here
-            throw new NotImplementedException();
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i].Length > array[j].Length)
+                    {
+                        string tmp = array[i];
+                        array[i] = array[j];
+                        array[j] = tmp;
+                    }
+                }
+            }
         }
     }
 }
